e0377f42f08fe903f63cee352a4d4680f02de944
shahbaz242630 <shahbaz.malik@hotmail.co.uk>
2026-08-12 15:03:51 +0400

    Fix Expo SDK dependency alignment

--- apps/mobile/modules/claimant-key-custody/src/index.ts
+++ apps/mobile/modules/claimant-key-custody/src/index.ts
@@ -1,4 +1,4 @@
-import { requireOptionalNativeModule } from "expo-modules-core";
+import { requireOptionalNativeModule } from "expo";
 
 export type NativeCustodyCapability = {
   result_class: string;
